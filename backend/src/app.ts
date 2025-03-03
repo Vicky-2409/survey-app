7159840a4ff70b7fb5c019e6b0e68df36fc4fdfa
Vignesh <svignesh2409@gmail.com>
2025-03-03 15:26:19 +0530

    added common package.json

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import path from "path";
 import { connectDB } from "./config/database";
 import surveyRoutes from "./routes/survey.routes";
 import authRoutes from "./routes/auth.routes";
@@ -22,6 +23,17 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/surveys", surveyRoutes);
 app.use("/api/auth", authRoutes);
+
+// Serve static frontend files in production
+if (process.env.NODE_ENV === 'production') {
+  // Serve static files from the React frontend app
+  app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+  
+  // Anything that doesn't match the above will be redirected to index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
+  });
+}
 
 app.use(
   (
